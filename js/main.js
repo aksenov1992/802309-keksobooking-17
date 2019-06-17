916725e7b800e3d72423f2a300a44ad1fa27aecd
aksenov1992 <mr.a.aksenov@gmail.com>
2019-06-17 15:40:59 +0700

    поправил пробелы

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var locationX = {
   min: 0,
   max: 10
 };
-var locationY  = {
+var locationY = {
   min: 0,
   max: 10
 };
@@ -19,42 +19,42 @@ var locationY  = {
 // генерация рандомных координат
 var generateRandomLocation = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
 // генерация адресов аватаров
-var generateAvatar = function() {
+var generateAvatar = function () {
   var usersAvatar = [];
   for (var i = 1; i <= 8; i++) {
     var avatars = 'img/avatars/user0' + i + '.png';
     usersAvatar.push(avatars);
-  };
+  }
   return usersAvatar[i];
 };
 
-//Выбор случайного типа дома из массива
+// Выбор случайного типа дома из массива
 var generateRandomHouse = function () {
   var randomHouse = Math.floor(Math.random() * houseType.length);
   return houseType[randomHouse];
-}
+};
 
-//Генерация объектов
+//  Генерация объектов
 
 var generateUsersFeature = function () {
   usersFeature.push(
-    { author: {
-        avatar: generateAvatar()
-      },
-      offer: {
-        type: generateRandomHouse()
-      },
-      location: {
-        x: generateRandomLocation(locationX.min, locationX.max),
-        y: generateRandomLocation(locationX.min, locationX.max)
+      {author: {
+          avatar: generateAvatar()
+        },
+        offer: {
+          type: generateRandomHouse()
+        },
+        location: {
+          x: generateRandomLocation(locationX.min, locationX.max),
+          y: generateRandomLocation(locationY.min, locationY.max)
+        }
       }
-    }
-  )
+  );
   return usersFeature;
-}
+};
 console.log(generateUsersFeature());
 // работа с шаблоном
 var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
