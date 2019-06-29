63ec76028ba7039d96488ded79e1ad4be9254825
aksenov1992 <mr.a.aksenov@gmail.com>
2019-06-29 16:10:03 +0700

    доделал до конца задания, исправил для тревиса

--- js/main.js
+++ js/main.js
@@ -19,30 +19,58 @@ var locationY = {
 
 // Поле «Тип жилья» влияет на минимальное значение поля «Цена за ночь»:
 
-var typeOfHouse = document.querySelector('#type');
-var pricePerNight = document.querySelector('#price');
+var typeOfHouse = adForm.querySelector('#type');
+var pricePerNight = adForm.querySelector('#price');
 
 // вешаем обработчик на select и пишем условия для минимальной цены от значчения типа дома
 typeOfHouse.addEventListener('change', function () {
   for (var i = 0; i < houseType.length; i++) {
     if (typeOfHouse.value === houseType[0]) {
       pricePerNight.min = 10000;
-      pricePerNight.placeholder = "10000";
-    };
+      pricePerNight.placeholder = '10000';
+    }
     if (typeOfHouse.value === houseType[1]) {
       pricePerNight.min = 1000;
-      pricePerNight.placeholder = "1000";
-    };
+      pricePerNight.placeholder = '1000';
+    }
     if (typeOfHouse.value === houseType[2]) {
       pricePerNight.min = 5000;
-      pricePerNight.placeholder = "5000";
-    };
+      pricePerNight.placeholder = '5000';
+    }
     if (typeOfHouse.value === houseType[3]) {
       pricePerNight.min = 0;
-      pricePerNight.placeholder = "0";
-    };
-  };
+      pricePerNight.placeholder = '0';
+    }
+  }
 });
+
+// Поля «Время заезда» и «Время выезда» синхронизированы
+var timeArrival = adForm.querySelector('#timein');
+var timeDeparture = adForm.querySelector('#timeout');
+
+timeArrival.addEventListener('change', function () {
+  if (timeArrival.value === '12:00') {
+    timeDeparture.value = '12:00';
+  }
+  if (timeArrival.value === '13:00') {
+    timeDeparture.value = '13:00';
+  }
+  if (timeArrival.value === '14:00') {
+    timeDeparture.value = '14:00';
+  }
+});
+
+timeDeparture.addEventListener('change', function () {
+  if (timeDeparture.value === '12:00') {
+    timeArrival.value = '12:00'
+  }
+  if (timeDeparture.value === '13:00') {
+    timeArrival.value = '13:00'
+  }
+  if (timeDeparture.value === '14:00') {
+    timeArrival.value = '14:00'
+  }
+})
 
 // повесил обработчик на указатель, активировал форму, задал нчальное значение адреса.
 adressForm.value = mapPin.offsetLeft + ', ' + mapPin.offsetTop;
