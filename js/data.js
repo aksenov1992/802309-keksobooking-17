da3172801f448a092157a747e034ee1f1cace4d4
aksenov1992 <mr.a.aksenov@gmail.com>
2019-07-04 17:00:20 +0700

    забыла про табы

--- js/data.js
+++ js/data.js
@@ -1,50 +1,50 @@
 'use strict';
 (function () {
-var usersFeature = [];
-var locationX = {
-  min: 0,
-  max: 10
-};
-var locationY = {
-  min: 0,
-  max: 10
-};
+  var usersFeature = [];
+  var locationX = {
+    min: 0,
+    max: 10
+  };
+  var locationY = {
+    min: 0,
+    max: 10
+  };
 
-// генерация рандомных координат
-var generateRandomLocation = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+  // генерация рандомных координат
+  var generateRandomLocation = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
 
-// генерация адресов аватаров
-var generateAvatar = function (index) {
-  return 'img/avatars/user0' + (index + 1) + '.png';
-};
+  // генерация адресов аватаров
+  var generateAvatar = function (index) {
+    return 'img/avatars/user0' + (index + 1) + '.png';
+  };
 
-// Выбор случайного типа дома из массива
-var generateRandomHouse = function () {
-  var randomHouse = Math.floor(Math.random() * window.form.houseType.length);
-  return houseType[randomHouse];
-};
+  // Выбор случайного типа дома из массива
+  var generateRandomHouse = function () {
+    var randomHouse = Math.floor(Math.random() * window.form.houseType.length);
+    return houseType[randomHouse];
+  };
 
-//  Генерация объектов
-var generateUsersFeature = function (index) {
-  usersFeature.push(
-      {author: {
-        avatar: generateAvatar(index)
-      },
-      offer: {
-        type: generateRandomHouse()
-      },
-      location: {
-        x: generateRandomLocation(locationX.min, locationX.max),
-        y: generateRandomLocation(locationY.min, locationY.max)
-      }
-      }
-  );
-  return usersFeature;
-};
+  //  Генерация объектов
+  var generateUsersFeature = function (index) {
+    usersFeature.push(
+        {author: {
+          avatar: generateAvatar(index)
+        },
+        offer: {
+          type: generateRandomHouse()
+        },
+        location: {
+          x: generateRandomLocation(locationX.min, locationX.max),
+          y: generateRandomLocation(locationY.min, locationY.max)
+        }
+        }
+    );
+    return usersFeature;
+  };
 
-for (var i = 0; i < 8; i++) {
-  generateUsersFeature(i);
-}
+  for (var i = 0; i < 8; i++) {
+    generateUsersFeature(i);
+  }
 })();
--- js/map.js
+++ js/map.js
@@ -1,70 +1,70 @@
 'use strict';
 (function () {
-var showMap = document.querySelector('.map');
-var mapPin = document.querySelector('.map__pin--main');
-var adForm = document.querySelector('.ad-form');
-var fieldsetForm = adForm.querySelectorAll('fieldset');
-var adressForm = adForm.querySelector('#address');
-  // повесил обработчик на указатель, активировал форму, задал нчальное значение адреса.
-adressForm.value = mapPin.offsetLeft + ', ' + mapPin.offsetTop;
-var mapActivation = function () {
-  if (adForm && showMap) {
-    adForm.classList.remove('ad-form--disabled');
-    showMap.classList.remove('map--faded');
-    for (var i = 0; i < fieldsetForm.length; i++) {
-      fieldsetForm[i].removeAttribute('disabled');
+  var showMap = document.querySelector('.map');
+  var mapPin = document.querySelector('.map__pin--main');
+  var adForm = document.querySelector('.ad-form');
+  var fieldsetForm = adForm.querySelectorAll('fieldset');
+  var adressForm = adForm.querySelector('#address');
+    // повесил обработчик на указатель, активировал форму, задал нчальное значение адреса.
+  adressForm.value = mapPin.offsetLeft + ', ' + mapPin.offsetTop;
+  var mapActivation = function () {
+    if (adForm && showMap) {
+      adForm.classList.remove('ad-form--disabled');
+      showMap.classList.remove('map--faded');
+      for (var i = 0; i < fieldsetForm.length; i++) {
+        fieldsetForm[i].removeAttribute('disabled');
+      }
     }
-  }
-};
-
-// Перемещения главного маркера (.map__pin--main) по карте.
-mapPin.addEventListener('mousedown', function (evt) {
-  var tokyoMap = document.querySelector('.map__overlay');
-  mapActivation();
-  var startPinCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-  var onMouseMove = function (moveEvt) {
-    var shift = {
-      x: startPinCoords.x - moveEvt.clientX,
-      y: startPinCoords.y - moveEvt.clientY
-    };
-    startPinCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-    var limitPinCoords = {
-      top: 130,
-      buttom: 630,
-      left: tokyoMap.offsetLeft,
-      right: tokyoMap.offsetWidth
-    };
-
-    var nowPinCoords = {
-      x: mapPin.offsetLeft - shift.x,
-      y: mapPin.offsetTop - shift.y,
-    };
-    if (nowPinCoords.y <= limitPinCoords.top) {
-      mapPin.style.top = limitPinCoords.top + 'px';
-    } else if (nowPinCoords.y >= limitPinCoords.buttom) {
-      mapPin.style.top = limitPinCoords.buttom + 'px';
-    }
-    if (nowPinCoords.x >= (limitPinCoords.right - mapPin.offsetWidth)) {
-      mapPin.style.left = limitPinCoords.right - mapPin.offsetWidth + 'px';
-    } else if (nowPinCoords.x <= limitPinCoords.left) {
-      mapPin.style.left = limitPinCoords.left + 'px';
-    }
-    mapPin.style.top = (mapPin.offsetTop - shift.y) + 'px';
-    mapPin.style.left = (mapPin.offsetLeft - shift.x) + 'px';
   };
 
-  var onMouseUp = function () {
-    adressForm.value = mapPin.offsetLeft + ', ' + mapPin.offsetTop;
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-  };
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+  // Перемещения главного маркера (.map__pin--main) по карте.
+  mapPin.addEventListener('mousedown', function (evt) {
+    var tokyoMap = document.querySelector('.map__overlay');
+    mapActivation();
+    var startPinCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var onMouseMove = function (moveEvt) {
+      var shift = {
+        x: startPinCoords.x - moveEvt.clientX,
+        y: startPinCoords.y - moveEvt.clientY
+      };
+      startPinCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      var limitPinCoords = {
+        top: 130,
+        buttom: 630,
+        left: tokyoMap.offsetLeft,
+        right: tokyoMap.offsetWidth
+      };
+
+      var nowPinCoords = {
+        x: mapPin.offsetLeft - shift.x,
+        y: mapPin.offsetTop - shift.y,
+      };
+      if (nowPinCoords.y <= limitPinCoords.top) {
+        mapPin.style.top = limitPinCoords.top + 'px';
+      } else if (nowPinCoords.y >= limitPinCoords.buttom) {
+        mapPin.style.top = limitPinCoords.buttom + 'px';
+      }
+      if (nowPinCoords.x >= (limitPinCoords.right - mapPin.offsetWidth)) {
+        mapPin.style.left = limitPinCoords.right - mapPin.offsetWidth + 'px';
+      } else if (nowPinCoords.x <= limitPinCoords.left) {
+        mapPin.style.left = limitPinCoords.left + 'px';
+      }
+      mapPin.style.top = (mapPin.offsetTop - shift.y) + 'px';
+      mapPin.style.left = (mapPin.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function () {
+      adressForm.value = mapPin.offsetLeft + ', ' + mapPin.offsetTop;
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
