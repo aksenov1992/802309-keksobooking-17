388024ad4acbcd8b6e0b07b614a39207f319c8fe
aksenov1992 <mr.a.aksenov@gmail.com>
2019-07-09 16:19:12 +0700

    поправил

--- js/form.js
+++ js/form.js
@@ -30,34 +30,13 @@
     var typeHouseArr = Object.keys(houseTypeAndPrice);
     for (var i = 0; i < typeHouseArr.length; i++) {
       if (typeHouseArr[i] === typeOfHouse.value) {
-        var linkObjPrice = eval('houseTypeAndPrice.' + typeHouseArr[i] + '.price');
+        var linkObjPrice = houseTypeAndPrice[typeHouseArr[i]].price;
         pricePerNight.min = linkObjPrice;
         pricePerNight.placeholder = pricePerNight.min;
       }
     }
   });
 
-    /*  typeOfHouse.addEventListener('change', function () {
-    for (var i = 0; i < houseType.length; i++) {
-      if (typeOfHouse.value === houseType[0]) {
-        pricePerNight.min = 10000;
-        pricePerNight.placeholder = '10000';
-      }
-      if (typeOfHouse.value === houseType[1]) {
-        pricePerNight.min = 1000;
-        pricePerNight.placeholder = '1000';
-      }
-      if (typeOfHouse.value === houseType[2]) {
-        pricePerNight.min = 5000;
-        pricePerNight.placeholder = '5000';
-      }
-      if (typeOfHouse.value === houseType[3]) {
-        pricePerNight.min = 0;
-        pricePerNight.placeholder = '0';
-      }
-    }
-  });
-*/
   // Поля «Время заезда» и «Время выезда» синхронизированы
 
   timeArrival.addEventListener('change', function () {
